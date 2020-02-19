cf3a11182315e9f401c67e32159fb485648cbfc5
Bernardo <bernardoc544@gmail.com>
2020-02-19 15:17:46 -0500

    My midterm test with C#

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+
+    private Rigidbody2D body2d;
+    private Animator animator;
+    private bool ground = false;
+    private bool right = true;
+    private bool crouch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 move = Vector2.zero;
+        float forward = Input.GetAxis("Horizontal");
+
+        if(ground && body2d.velocity.x == 0 && Input.GetAxis("Vertical") < 0)
+        {
+
+        }
+    }
+}
