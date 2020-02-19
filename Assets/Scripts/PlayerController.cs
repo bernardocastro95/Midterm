07798ed471dde7a0952de52c37050fcb3f979656
Bernardo <bernardoc544@gmail.com>
2020-02-19 16:08:57 -0500

    Codding for Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jump = 500.0f;
+    [SerializeField] private Transform groundPosition;
+    [SerializeField] private LayerMask whatGround;
+    [SerializeField] private float radius;
 
 
     private Rigidbody2D body2d;
@@ -25,9 +29,50 @@ public class PlayerController : MonoBehaviour
         Vector2 move = Vector2.zero;
         float forward = Input.GetAxis("Horizontal");
 
+        ground = checkingIfInGround();
+
+        if(ground && Input.GetAxis("Jump") > 0)
+        {
+            body2d.AddForce(new Vector2(0.0f, jump));
+            ground = false;
+        }
+        
+        body2d.velocity = new Vector2(forward * speed, body2d.velocity.y) ;
+
         if(ground && body2d.velocity.x == 0 && Input.GetAxis("Vertical") < 0)
         {
-
+            crouch = true;
         }
+        else
+        {
+            crouch = false;
+        }
+
+        if (right && body2d.velocity.x < 0)
+        {
+            flipping();
+        }
+        else if(!right && body2d.velocity.x > 0)
+        {
+            flipping();
+        }
+
+        animator.SetFloat("speedX", Mathf.Abs(body2d.velocity.x));
+        animator.SetFloat("speedY", Mathf.Abs(body2d.velocity.y));
+        animator.SetBool("ground", ground);
+        animator.SetBool("crouch", crouch);
+    }
+    
+    private bool checkingIfInGround()
+    {
+        return Physics2D.OverlapCircle(groundPosition.position, radius, whatGround);
+    }
+    private void flipping()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        right = !right;
     }
 }
